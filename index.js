9f93fdd2c23d7c9b1cf5afc0464d6fed4f4bdb03
Ariful4593 <arifkhagoria2015@gmail.com>
2020-09-29 19:37:03 +0600

    Updated

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
 
-
-const app = express()
-// const rootCall = (req, res) => res.send('Thank you so Much!')
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) =>{
-    res.send('Thank you for calling me')
+    const fruits = {
+        product: 'ada',
+        price: 500,
+        weight: 300,
+    }
+    res.send(fruits)
+})
+app.get('/fruits/banana', (req, res) =>{
+    const banaa = {
+        product: 'banana',
+        price: 100,
+        hali: 3
+    }
+    res.send(banaa)
 })
 
-app.listen(3000, () => console.log('Listening to port 3000'))
+const user = ['arif', 'selim', 'karim', 'fahad','kaium']
+app.get('/user/:id', (req, res) =>{
+    const userId = req.params.id;
+    const name = user[userId]
+    res.send({userId, name})
+    // console.log(userId)
+})
+
+app.post('/addUser', (req, res) =>{
+    // console.log('data received: ',req.body)
+    const user = req.body;
+    user.id = 55;
+    res.send(user)
+})
+
+app.listen(3000, () => console.log("Listening port 3000"))
